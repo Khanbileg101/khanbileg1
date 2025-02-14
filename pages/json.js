3465b4e44342ee3acf250f0f25fd0e5cc3805717
Zutguur <khanbileg7@gmail.com>
2025-02-14 13:27:42 +0800

    json

--- pages/json.js
+++ pages/json.js
@@ -7,36 +7,35 @@ export default function Json() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("https://mongol-api-rest.vercel.app/Instruments");
+                const response = await fetch('https://mongol-api-rest.vercel.app/clothes');
                 const json = await response.json();
                 setData(json);
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error(error);
             }
         };
 
         fetchData();
     }, []);
 
+    console.log(data.clothes);
+
     return (
-        <div className={`bg-${color}-800 h-screen`}>
-            {data && data.map((item) => (
-                <div key={item?.id}>{item?.id}</div>
+        <div className="min-h-screen bg-red-100 p-5 flex flex-col items-center">
+          <div className=" grid grid-cols-4 gap-4 m-4">
+            {data?.clothes?.map((item) => (
+                <div  key={item.id} className=" border-2 text-black bg-white shadow rounded p-4 items-center text-center">
+                    <img 
+                      src={item?.images[0]} 
+                      className="w-full h-64 rounded-xl"                  
+                    />
+                    <p className="font-bold text-center">{item.name}</p> 
+                    <p>{item.description}</p>
+                    <p className="font-bold">{item.timePeriod}</p>
+                    <p className="font-bold">{item?.materials}</p>
+                </div>
             ))}
-            <button className="bg-red-800 border p-4 m-4"
-            onClick={() => setColor("red")}>
-                red
-            </button>
-
-            <button className="bg-green-800 border p-4 m-4"
-            onClick={() => setColor("green")}>
-                green
-            </button>
-
-            <button className="bg-blue-800 border p-4 m-4"
-            onClick={() => setColor("blue")}>
-                blue
-            </button>
+        </div>
         </div>
     )
 }
