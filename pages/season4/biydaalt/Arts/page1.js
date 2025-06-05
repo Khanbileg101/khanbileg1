1b477a0b4c8f4ec857addd9cbf8eb5f187dcc667
khanbileg <khanbileg7@gmail.com>
2025-06-05 21:53:49 +0800

    arts

--- pages/season4/biydaalt/Arts/page1.js
+++ pages/season4/biydaalt/Arts/page1.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 
-export default function Engineer() {
+export default function Arts() {
   const router = useRouter();
   return (
     <div className="min-h-screen w-full flex items-center justify-center p-2 sm:p-4">
-      <div className="w-full max-w-[390px] h-[100vh] sm:h-[844px] bg-[#F9BC5D] relative rounded-3xl overflow-hidden shadow-lg">
+      <div className="w-full max-w-[390px] h-[100vh] sm:h-[844px] bg-[#DF6E5B] relative rounded-3xl overflow-hidden shadow-lg">
         <div className="text-white text-4xl sm:text-5xl font-bold text-center mt-12 sm:mt-24">
-          Engineer
+          Arts
         </div>
 
         <button 
@@ -20,19 +20,19 @@ export default function Engineer() {
         <div className="bg-white absolute bottom-0 w-full h-[75%] sm:h-[77%] rounded-t-[40px] p-4 sm:p-6 pt-6 sm:pt-8">
           <div className="w-full mb-6 sm:mb-10">
             <img
-              src="/image 21.png"
+              src="/arts1.png"
               alt="Plant illustration"
               className="w-[90%] h-[280px] sm:h-[340px] object-contain mx-auto"
             />
           </div>
 
           <p className="text-[28px] sm:text-[36px] text-black mb-6 sm:mb-8 text-center">
-            🗼 Build a Tower
+            Color Mixing Fun! 🎨
           </p>
 
           <div className="w-full flex justify-center">
             <button 
-              onClick={() => router.push("/season4/biydaalt/Engineer/page2")}
+              onClick={() => router.push("/season4/biydaalt/Arts/page2")}
               className="bg-[#feb40d] w-[90%] py-4 sm:py-6 rounded-full text-white text-3xl sm:text-4xl font-bold shadow-md hover:brightness-110 transition"
             >
               Start
--- pages/season4/biydaalt/Arts/page4.js
+++ pages/season4/biydaalt/Arts/page4.js
@@ -5,21 +5,21 @@ export default function Engineer() {
   const router = useRouter();
   return (
     <div className="min-h-screen w-full flex items-center justify-center p-2 sm:p-4">
-      <div className="w-full max-w-[390px] h-[100vh] sm:h-[844px] bg-[#F9BC5D] relative rounded-3xl overflow-hidden shadow-lg">
+      <div className="w-full max-w-[390px] h-[100vh] sm:h-[844px] bg-[#DF6E5B] relative rounded-3xl overflow-hidden shadow-lg">
         <div className="w-full flex justify-center mt-8 sm:mt-12">
           <img
-            src="/image 3.png"
+            src="/image 4.png"
             alt="Science Illustration Above"
             className="w-[90%] sm:w-auto object-contain"
           />
         </div>
 
         <div className="text-white text-4xl sm:text-5xl font-bold text-center mt-4">
-          Engineer
+          Arts
         </div>
 
         <button 
-          onClick={() => router.push("/season4/biydaalt/Engineer/page1")} 
+          onClick={() => router.push("/season4/biydaalt/Arts/page3")} 
           className="absolute top-6 sm:top-8 left-4 w-10 h-10 flex items-center justify-center"
         >
           <img src="/Group 28.png" alt="Back" className="w-8 sm:w-10 h-8 sm:h-10" />
@@ -28,22 +28,23 @@ export default function Engineer() {
         <div className="bg-white absolute bottom-0 w-full h-[70%] rounded-t-[40px] p-4 sm:p-6 pt-6 sm:pt-8">
           <div className="w-full mb-6 sm:mb-10">
             <img
-              src="/image 22.png"
+              src="/Rectangle 33.png"
               alt="Science illustration"
-              className="object-contain mx-auto"
+              className="h-[280px] sm:h-[300px] object-contain mx-auto"
             />
           </div>
 
-          <p className="text-[18px] sm:text-[21px] font-medium text-black mb-6 sm:mb-8 text-center leading-relaxed">
-            Step 1: Start with a Strong Base🏗️
+          <p className="text-[20px] sm:text-[20px] font-medium text-black mb-6 sm:mb-8 text-center leading-relaxed">
+            🎉 Fun Tip:
+            If you mix all three colors together, you get a brown color!
           </p>
 
           <div className="w-full flex justify-center">
             <button
-              onClick={() => router.push("/season4/biydaalt/Engineer/page3")}
+              onClick={() => router.push("/")}
               className="bg-[#feb40d] w-[90%] py-4 sm:py-6 rounded-full text-white text-3xl sm:text-4xl font-bold shadow-md hover:brightness-110 transition"
             >
-              Continue
+              End
             </button>
           </div>
         </div>
